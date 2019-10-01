dfd805a4855debe88e6d03effcb64d8fff4d97af
1612421 <nguyenngocnghia53@gmail.com>
2019-10-01 22:01:22 +0700

    Updates

---
+++ precache-manifest.df4e5a3169269f645f609ab557eb9508.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8aad97cec2a668bb9ecf3945e55a91be",
+    "url": "/Caro/index.html"
+  },
+  {
+    "revision": "0bbac633fe98c1986eb2",
+    "url": "/Caro/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "cba3abc125505898c1ea",
+    "url": "/Caro/static/css/main.ca04d9d1.chunk.css"
+  },
+  {
+    "revision": "0bbac633fe98c1986eb2",
+    "url": "/Caro/static/js/2.9a6a2fde.chunk.js"
+  },
+  {
+    "revision": "cba3abc125505898c1ea",
+    "url": "/Caro/static/js/main.d45988f5.chunk.js"
+  },
+  {
+    "revision": "058b22e644cbef08532a",
+    "url": "/Caro/static/js/runtime~main.439958b1.js"
+  }
+]);
